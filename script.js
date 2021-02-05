13dda7f7e1a9c5570f09081412508c25361ea869
Nazim <halilov.nazim72@gmail.com>
2021-02-05 18:42:11 +0300

    '2-lesson'

--- script.js
+++ script.js
@@ -1,19 +1,116 @@
-// Конвертер из Цельсия в Фаренгейт
+/*
+var a = 1, b = 1, c, d;
+c = ++a; alert(c);           // 2, так как ++ стоит до a, то в a присваиваться 2 а потом выводиться c
+d = b++; alert(d);           // 1, здесь наоборот сначала выводиться, а потом присваивается в b = 2 
+c = (2+ ++a); alert(c);      // 5, такой же подход как и в первых вычеслениях
+d = (2+ b++); alert(d);      // 4, такой же подход как и в первых вычеслениях
+alert(a);                    // 3
+alert(b);                    // 3
+*/
 
-let tempC = +prompt('Введите температура в градусах Цельсия: ');
-let tempF = 9/5 * tempC + 32;
+/* 
+var a = 2; 
+var x = 1 + (a *= 2); // (a *= 2)  = 4 сначала вычисляется это, далее прибавляется 1 ==> x = 5
+*/
 
-alert(tempF);
+/*
+var a = 15, b = -4;
+if(a >= 0 && b >= 0){
+    alert(a - b);
+} else if (a < 0 && b < 0){
+    alert(a * b);
+} else {
+    alert(a + b);
+}
+*/
 
-// Переменные
+/*
+a = +prompt ('Введите число от 1 до 15');
+		switch (a) {
+		case 1:
+			alert ( 'Ваше число 1');
+			break;
+		case 2:
+			alert ( 'Ваше число 2');
+			break;
+		case 3:
+			alert ( 'Ваше число 3');
+			break;
+		case 4:
+			alert ( 'Ваше число 4');
+			break;
+		case 5:
+			alert ( 'Ваше число 5');
+			break;
+		case 6:
+			alert ( 'Ваше число 6');
+			break;
+		case 7:
+			alert ( 'Ваше число 7');
+			break;
+		case 8:
+			alert ( 'Ваше число 8');
+			break;
+		case 9:
+			alert ( 'Ваше число 9');
+			break;
+		case 10:
+			alert ( 'Ваше число 10');
+			break;
+		case 11:
+			alert ( 'Ваше число 11');
+			break;
+		case 12:
+			alert ( 'Ваше число 12');
+			break;
+		case 13:
+			alert ( 'Ваше число 13');
+			break;
+		case 14:
+			alert ( 'Ваше число 14');
+			break;
+		case 15:
+			alert ( 'Ваше число 15');
+			break;	
+		}
+*/
 
-let name = "Василий";
-let admin = name;
+/*
+    var a = 2;
+    var b = 3;
 
-alert(admin); // "Василий"
+	function plus(a, b) {
+    return a + b;
+	}
+    
+	function minus(a, b) {
+    return a - b;
+	}
 
-alert(1000 + "108"); // 1000108
+    function div (a, b) {
+    return a / b;
+    }
 
-//async запускает скрипт асинхронно
+    function mult (a, b) {
+	return a + b;
+	}
+*/
 
-//defer откладывает выполнение скрипта до тех пор, пока вся страница не будет загружена полностью
+/*
+function mathOperation(arg1, arg2, operation) {
+    switch (operation) {
+        case 'сложение':
+            return arg1 + arg2;
+            break;
+        case 'вычитание':
+            return arg1 - arg2;
+            break;
+        case 'деление':
+            return arg1 / arg2;
+            break;
+        case 'умножение':
+            return arg1 * arg2;
+            break;
+    }
+}
+*/
